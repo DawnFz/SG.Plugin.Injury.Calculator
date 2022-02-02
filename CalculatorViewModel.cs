eb9c6f46230a48adf4cac621b5606e80a70c9302
DismissedLight <1686188646@qq.com>
2022-02-02 15:05:19 +0800

    fix UI style

--- CalculatorViewModel.cs
+++ CalculatorViewModel.cs
@@ -1,45 +1,22 @@
 ﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
 using Snap.Core.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Windows;
-using System.Windows.Media;
+using System.Windows.Input;
 
 namespace Injury.Calculator.Plugin
 {
     [ViewModel(InjectAs.Transient)]
     internal class CalculatorViewModel : ObservableObject
     {
-        private IEnumerable<object> icons;
-
-        public IEnumerable<object> Icons { get => icons; set => SetProperty(ref icons, value); }
-
         public CalculatorViewModel()
         {
-            List<object>? list = new();
-            ICollection<FontFamily>? families = Fonts.GetFontFamilies(@"C:\Windows\Fonts\segmdl2.ttf");
-            foreach (FontFamily family in families)
-            {
-                ICollection<Typeface>? typefaces = family.GetTypefaces();
-                foreach (Typeface typeface in typefaces)
-                {
-                    typeface.TryGetGlyphTypeface(out GlyphTypeface glyph);
-                    IDictionary<int, ushort> characterMap = glyph.CharacterToGlyphMap;
-
-                    foreach (KeyValuePair<int, ushort> kvp in characterMap)
-                    {
-                        list.Add(new { Glyph = (char)kvp.Key, Data = kvp.Key });
-                    }
-                }
-            }
-            icons = list;
-            CountDamageCommand = new DelegateCommand { ExecuteAction = new Action<object>(CountDamage) };
-            data = new();
+            CountDamageCommand = new RelayCommand(CountDamage);
+            Data = new();
         }
-        private DatasModel _data;
-        public DatasModel data
+        private DatasModel? _data;
+        public DatasModel Data
         {
-            get => _data;
+            get => _data!;
             set => SetProperty(ref _data, value);
         }
 
@@ -64,77 +41,77 @@ namespace Injury.Calculator.Plugin
         private int _ZDM;
         public int ZDM { get => _ZDM; set => SetProperty(ref _ZDM, value); }
 
-        public DelegateCommand CountDamageCommand { get; set; }
-        private void CountDamage(object parameter)
+        public ICommand CountDamageCommand { get; set; }
+        private void CountDamage()
         {
             double D_RR;
-            double EM = 1 + ((278 * data.Elemental_Mystery) / (data.Elemental_Mystery + 1400)) / 100;//精通转换独立乘区系数
-            double DMG = 1 + (data.CRIT_DMG / 100);//爆伤乘区
-            double DEF = (data.Role_Level + 100) / (data.Monster_Level + 100 + (data.Monster_Level + 100) * (1 - (data.Reduce_Defense / 100)));//减防公式
-            double D_M = data.Multiple / 100;//倍率百分比转化
-            double D_EA = 1 + (data.Element_Addition / 100);//伤害加成增伤公式
-            double ER = data.Charge_Efficiency * 0.25;
-            if (data.Reduce_Resistance / 100 > 0.1)//怪物减抗是否为负判断
+            double EM = 1 + ((278 * Data.Elemental_Mystery) / (Data.Elemental_Mystery + 1400)) / 100;//精通转换独立乘区系数
+            double DMG = 1 + (Data.CRIT_DMG / 100);//爆伤乘区
+            double DEF = (Data.Role_Level + 100) / (Data.Monster_Level + 100 + (Data.Monster_Level + 100) * (1 - (Data.Reduce_Defense / 100)));//减防公式
+            double D_M = Data.Multiple / 100;//倍率百分比转化
+            double D_EA = 1 + (Data.Element_Addition / 100);//伤害加成增伤公式
+            double ER = Data.Charge_Efficiency * 0.25;
+            if (Data.Reduce_Resistance / 100 > 0.1)//怪物减抗是否为负判断
             {
-                D_RR = 1 - (0.1 - (data.Reduce_Resistance / 100) / 2);
+                D_RR = 1 - (0.1 - (Data.Reduce_Resistance / 100) / 2);
             }
             else
             {
-                D_RR = 1 - (0.1 - (data.Reduce_Resistance / 100));
+                D_RR = 1 - (0.1 - (Data.Reduce_Resistance / 100));
             }
 
             if (TypeIndex == 0 && EffectIndex == 0)//无增幅+无套装
             {
-                YDM = (int)(data.Atks * DMG * D_EA * D_M * DEF * D_RR);
-                XDM = (int)(data.Atks * D_EA * D_M * DEF * D_RR);
+                YDM = (int)(Data.Atks * DMG * D_EA * D_M * DEF * D_RR);
+                XDM = (int)(Data.Atks * D_EA * D_M * DEF * D_RR);
                 
             }
             else if (TypeIndex == 0 && EffectIndex == 1)//无增幅+魔女4
             {
-                YDM = (int)(data.Atks * DMG * (D_EA + 0.075) * D_M * DEF * D_RR);
-                XDM = (int)(data.Atks * (D_EA + 0.075) * D_M * DEF * D_RR);
+                YDM = (int)(Data.Atks * DMG * (D_EA + 0.075) * D_M * DEF * D_RR);
+                XDM = (int)(Data.Atks * (D_EA + 0.075) * D_M * DEF * D_RR);
 
             }
             else if (TypeIndex == 0 && EffectIndex == 2)//无增幅+绝缘4
             {
                 if (ER > 0.75)
                     ER = 0.75;
-                YDM = (int)(data.Atks * DMG * (D_EA+ER) * D_M * DEF * D_RR);
-                XDM = (int)(data.Atks * (D_EA+ER) * D_M * DEF * D_RR);
+                YDM = (int)(Data.Atks * DMG * (D_EA+ER) * D_M * DEF * D_RR);
+                XDM = (int)(Data.Atks * (D_EA+ER) * D_M * DEF * D_RR);
             }
             else if (TypeIndex == 1 && EffectIndex == 0)//1.5倍+无套装
             {
-                YDM = (int)(data.Atks * DMG * (D_EA + 0.075) * D_M * DEF * D_RR * EM * 1.5);
-                XDM = (int)(data.Atks * (D_EA + 0.075) * D_M * DEF * D_RR * EM * 1.5);
+                YDM = (int)(Data.Atks * DMG * (D_EA + 0.075) * D_M * DEF * D_RR * EM * 1.5);
+                XDM = (int)(Data.Atks * (D_EA + 0.075) * D_M * DEF * D_RR * EM * 1.5);
             }
             else if (TypeIndex == 1 && EffectIndex == 1)//1.5倍+魔女4
             {
-                YDM = (int)(data.Atks * DMG * (D_EA + 0.075) * D_M * DEF * D_RR * (EM + 0.15) * 1.5);
-                XDM = (int)(data.Atks * (D_EA + 0.075) * D_M * DEF * D_RR * (EM + 0.15) * 1.5);
+                YDM = (int)(Data.Atks * DMG * (D_EA + 0.075) * D_M * DEF * D_RR * (EM + 0.15) * 1.5);
+                XDM = (int)(Data.Atks * (D_EA + 0.075) * D_M * DEF * D_RR * (EM + 0.15) * 1.5);
             }
             else if (TypeIndex == 1 && EffectIndex == 2)//1.5倍+绝缘4
             {
                 if (ER > 0.75)
                     ER = 0.75;
-                YDM = (int)(data.Atks * DMG * (D_EA + ER) * D_M * DEF * D_RR*EM*1.5);
-                XDM = (int)(data.Atks * (D_EA + ER) * D_M * DEF * D_RR*EM*1.5);
+                YDM = (int)(Data.Atks * DMG * (D_EA + ER) * D_M * DEF * D_RR*EM*1.5);
+                XDM = (int)(Data.Atks * (D_EA + ER) * D_M * DEF * D_RR*EM*1.5);
             }
             else if (TypeIndex == 2 && EffectIndex == 0)//2倍+无套装
             {
-                YDM = (int)(data.Atks * DMG * (D_EA + 0.075) * D_M * DEF * D_RR * EM * 2);
-                XDM = (int)(data.Atks * (D_EA + 0.075) * D_M * DEF * D_RR * EM * 2);
+                YDM = (int)(Data.Atks * DMG * (D_EA + 0.075) * D_M * DEF * D_RR * EM * 2);
+                XDM = (int)(Data.Atks * (D_EA + 0.075) * D_M * DEF * D_RR * EM * 2);
             }
             else if (TypeIndex == 2 && EffectIndex == 1)//2倍+魔女4
             {
-                YDM = (int)(data.Atks * DMG * (D_EA + 0.075) * D_M * DEF * D_RR * (EM + 0.15) *2);
-                XDM = (int)(data.Atks * (D_EA + 0.075) * D_M * DEF * D_RR * (EM + 0.15) * 2);
+                YDM = (int)(Data.Atks * DMG * (D_EA + 0.075) * D_M * DEF * D_RR * (EM + 0.15) *2);
+                XDM = (int)(Data.Atks * (D_EA + 0.075) * D_M * DEF * D_RR * (EM + 0.15) * 2);
             }
             else if (TypeIndex == 2 && EffectIndex == 2)//2倍+绝缘4
             {
                 if (ER > 0.75)
                     ER = 0.75;
-                YDM = (int)(data.Atks * DMG * (D_EA + ER) * D_M * DEF * D_RR * EM * 2);
-                XDM = (int)(data.Atks * (D_EA + ER) * D_M * DEF * D_RR * EM * 2);
+                YDM = (int)(Data.Atks * DMG * (D_EA + ER) * D_M * DEF * D_RR * EM * 2);
+                XDM = (int)(Data.Atks * (D_EA + ER) * D_M * DEF * D_RR * EM * 2);
             }
         }
     }
--- DatasModel.cs
+++ DatasModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Injury.Calculator.Plugin
+﻿namespace Injury.Calculator.Plugin
 {
     public class DatasModel
     {
