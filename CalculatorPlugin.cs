febf618d63076dbbcfc72cfeb45cdc37d39b1ea0
ねねだん <admin@dawnfz.com>
2022-04-26 13:55:12 +0800

    fix bugs

--- CalculatorPlugin.cs
+++ CalculatorPlugin.cs
@@ -1,5 +1,6 @@
 ﻿using DGP.Genshin.Core.Plugins;
 using System;
+using System.Windows.Input;
 
 [assembly: SnapGenshinPlugin]
 
@@ -11,10 +12,10 @@ namespace Injury.Calculator.Plugin
     [ImportPage(typeof(CalculatorPage), "伤害计算器", "\uE94C")]
     public class CalculatorPlugin : IPlugin
     {
-        public string Name => "伤害计算器 - 1.0.3";
-        public string Description => "本插件用于根据角色属性面板粗略计算大致伤害，有些拥有独立乘区的角色可能计算结果会有较大偏差，实际伤害请以游戏内为准，本版本为先行版，可能存在一些问题，如发现或有建议请联系开发者";
+        public string Name => "伤害计算器";
+        public string Description => "用于根据角色属性面板粗略计算大致伤害，拥有独立乘区的角色可能计算结果会有较大偏差，实际伤害以游戏内为准，如发现或有建议请联系开发者";
         public string Author => "DawnFz（ねねだん）";
-        public Version Version => new("1.0.3.0");
+        public Version Version => new("1.0.5.0");
 
         /// <summary>
         /// 可以使用 Snap Genshin 内置的设置服务储存
@@ -23,7 +24,22 @@ namespace Injury.Calculator.Plugin
         public bool IsEnabled
         {
             get;
-            set;
         }
+        #region IPlugin2
+        public string? DetailLink
+        {
+            get => "https://github.com/DawnFz/SG.Plugin.Injury.Calculator";
+        }
+
+        public bool IsSettingSupported
+        {
+            get => false;
+        }
+
+        public ICommand SettingCommand
+        {
+            get => null!;
+        }
+        #endregion
     }
 }
